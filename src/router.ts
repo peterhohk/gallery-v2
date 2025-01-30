04a06ae63edaec2235870cb1876e3ba6a25d3890
peterhohk <peterhohk0409@gmail.com>
2025-01-30 18:02:08 +0800

    try another history mode

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import FeaturedView from "./views/FeaturedView.vue";
 import CharactersView from "./views/CharactersView.vue";
 import ArtworksView from "./views/ArtworksView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   linkActiveClass: "header__nav-link--active",
   linkExactActiveClass: "header__nav-link--active",
   scrollBehavior: (to, from, savedPosition) => {
