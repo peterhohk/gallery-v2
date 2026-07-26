e5d8d034ff1a5626bf3bff9b9167d6c33d3faa22
peterhohk <129306968+peterhohk@users.noreply.github.com>
2026-07-26 15:47:10 +0800

    huh?

--- src/components/ArtworkButton.vue
+++ src/components/ArtworkButton.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { Artwork } from '@/models/artwork';
+import type { Artwork } from "@/models/artwork";
 
 const { artwork } = defineProps<{
   artwork: Artwork,
